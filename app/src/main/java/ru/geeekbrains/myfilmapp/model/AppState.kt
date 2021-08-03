2df16164b88cdbcfa9be66a9f9ed7c30923218d6
PrinceSch <sofy95@mail.ru>
2021-08-04 01:33:08 +0700

    Добавьте экран с описанием конкретного фильма по аналогии с погодным приложением.

--- app/src/main/java/ru/geeekbrains/myfilmapp/model/AppState.kt
+++ app/src/main/java/ru/geeekbrains/myfilmapp/model/AppState.kt
@@ -3,7 +3,7 @@ package ru.geeekbrains.myfilmapp.model
 import ru.geeekbrains.myfilmapp.model.data.Film
 
 sealed class AppState {
-    data class Success(val filmData: Film) : AppState()
+    data class Success(val filmData: List<Film>) : AppState()
     class Error (val error : Throwable) : AppState()
     object Loading : AppState()
 }
--- app/src/main/java/ru/geeekbrains/myfilmapp/model/repository/RepositoryImpl.kt
+++ app/src/main/java/ru/geeekbrains/myfilmapp/model/repository/RepositoryImpl.kt
@@ -1,13 +1,26 @@
 package ru.geeekbrains.myfilmapp.model.repository
 
 import ru.geeekbrains.myfilmapp.model.data.Film
+import ru.geeekbrains.myfilmapp.model.data.Genre
+import ru.geeekbrains.myfilmapp.model.data.getFantasyFilms
+import ru.geeekbrains.myfilmapp.model.data.getMarvelFilms
 
 class RepositoryImpl : Repository {
     override fun getFilmFromServer(): Film {
-        return Film("Harry Potter", 1, null)
+        return Film("Гарри Поттер и философский камень",
+            listOf(Genre(12, "приключения"), Genre(14, "фэнтези")),
+            671,
+            "Harry Potter and the Philosopher's Stone",
+            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8i7H09Bj25V6ZxaKWybvUYZmyT6.jpg")
     }
 
-    override fun getFilmFromLocal(): Film {
-        return Film("The Hitchhiker's Guide to the Galaxy", 42, null)
+    override fun getFilmFromLocalFantasy(): List<Film> {
+        return getFantasyFilms()
     }
+
+    override fun getFilmFromLocalMarvel(): List<Film> {
+        return getMarvelFilms()
+    }
+
+
 }
